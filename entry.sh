4322afdfd39f2e8e18ca95d640dbce0b3e8f8d98
syoch <hacksyoch@gmail.com>
2021-06-28 22:13:59 +0900

    update: add path register

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
-# PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
+PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 
 powerpc-eabi-gcc -Os -nostdlib -c raw.cpp -o raw.o
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
