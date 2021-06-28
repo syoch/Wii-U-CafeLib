1121b257c6da90d7286aebd5587d6f9cf5cb71a7
syoch <hacksyoch@gmail.com>
2021-06-28 17:54:58 +0900

    update: add test code

--- raw.cpp
+++ raw.cpp
@@ -1,3 +1,11 @@
+#include "rawFunc.cpp"
+
+namespace Minecraft
+{
+  rawFunc<uint32_t *, 0x03166818> getInstance;
+} // namespace Minecraft
+
 void code()
 {
+  *((uint32_t **)0x20000000) = Minecraft::getInstance();
 }
