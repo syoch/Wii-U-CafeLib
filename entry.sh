b6750fdbd3114635f7d273b93e1d4ecb51484f8d
syoch <hacksyoch@gmail.com>
2021-06-28 22:14:11 +0900

    update: add assembly output

--- entry.sh
+++ entry.sh
@@ -4,6 +4,7 @@ PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 
 powerpc-eabi-gcc -Os -nostdlib -c raw.cpp -o raw.o
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
+powerpc-eabi-objdump -d raw.o | c++filt
 hexdump \
   -e '4/1 "%02x" " " 4/1 "%02x" "\n"' \
   raw.bin
