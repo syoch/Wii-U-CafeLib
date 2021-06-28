3f92860ced0c0c513df435c5a1fcffd7d6085baf
syoch <hacksyoch@gmail.com>
2021-06-28 17:54:46 +0900

    update :simplize script

--- entry.sh
+++ entry.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-gcc -Os -nostdlib -c raw.cpp -o raw.o
-/opt/devkitpro/devkitPPC/bin/powerpc-eabi-objdump -d raw.o
+PATH=$PATH:/opt/devkitpro/devkitPPC/bin/
+
+powerpc-eabi-gcc -Os -nostdlib -c raw.cpp -o raw.o
+powerpc-eabi-objdump -d raw.o
