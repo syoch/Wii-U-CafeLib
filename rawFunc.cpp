1e334603233fb5549f8d3bbf49debfa99caa444a
syoch <hacksyoch@gmail.com>
2021-06-29 23:25:40 +0900

    update: remove dynamic arg system

--- rawFunc.cpp
+++ rawFunc.cpp
@@ -4,18 +4,7 @@ template <typename ret, uint32_t addr, typename... Args>
 class rawFunc
 {
 public:
-  ret operator()(Args... args)
-  {
-    return ((ret(*)(Args...))(addr))(args...);
-  }
-};
-
-template <typename ret, uint32_t addr>
-class rawFunc<ret, addr>
-{
-public:
-  template <typename... Args>
-  ret operator()(Args... args)
+  inline ret operator()(Args... args)
   {
     return ((ret(*)(Args...))(addr))(args...);
   }
