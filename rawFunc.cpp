733e0ed4186f43a68e30e7d2be820c1c85952110
syoch <hacksyoch@gmail.com>
2021-06-29 21:44:18 +0900

    update: add typed rawFunc

--- rawFunc.cpp
+++ rawFunc.cpp
@@ -1,6 +1,17 @@
 #include <stdint.h>
-template <typename ret, uint32_t addr>
+
+template <typename ret, uint32_t addr, typename... Args>
 class rawFunc
+{
+public:
+  ret operator()(Args... args)
+  {
+    return ((ret(*)(Args...))(addr))(args...);
+  }
+};
+
+template <typename ret, uint32_t addr>
+class rawFunc<ret, addr>
 {
 public:
   template <typename... Args>
