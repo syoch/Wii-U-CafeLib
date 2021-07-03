79a89337296756e5fd328e8ddfd4367005712b71
syoch <hacksyoch@gmail.com>
2021-07-03 20:42:42 +0900

    refactor: add endlie

--- entry.sh
+++ entry.sh
@@ -2,7 +2,14 @@
 cd $(dirname $0)
 PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 
-powerpc-eabi-gcc -nostdlib -O3 raw.cpp -Wl,-T,link.ld -o raw.o || exit
+powerpc-eabi-gcc \
+  -nostdlib \
+  -O3 \
+  raw.cpp \
+  -Wl,-T,link.ld \
+  -o raw.o \
+|| exit
+
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
 powerpc-eabi-objdump \
   --section=.init \
