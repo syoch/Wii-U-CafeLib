ae07daf85fd7bdf107494fc2d0905416dfd40909
syoch <hacksyoch@gmail.com>
2021-07-03 18:56:11 +0900

    update: change option

--- entry.sh
+++ entry.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 
-powerpc-eabi-gcc -O3 -c raw.cpp -o raw.o || exit
+powerpc-eabi-gcc -nostdlib -O3 raw.cpp -Wl,-T,link.ld -o raw.o || exit
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
 powerpc-eabi-objdump -h raw.o | c++filt
 hexdump \
