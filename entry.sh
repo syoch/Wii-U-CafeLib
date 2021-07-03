a1aabb8f562606d3584f9fab8aea34afb70e48d0
syoch <hacksyoch@gmail.com>
2021-07-03 19:33:12 +0900

    update: add linker script

--- entry.sh
+++ entry.sh
@@ -5,6 +5,7 @@ PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 powerpc-eabi-gcc -nostdlib -O3 raw.cpp -Wl,-T,link.ld -o raw.o || exit
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
 powerpc-eabi-objdump -h raw.o | c++filt
+powerpc-eabi-objdump -d raw.o | c++filt
 hexdump \
   -e '4/1 "%02x" " " 4/1 "%02x" "\n"' \
   raw.bin | xsel --clipboard --input
