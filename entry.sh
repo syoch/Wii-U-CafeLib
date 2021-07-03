209adb4d7936c2aeca1f7edbfb4b365cd7cc3d70
syoch <hacksyoch@gmail.com>
2021-07-03 20:41:55 +0900

    update: .init debug

--- entry.sh
+++ entry.sh
@@ -4,7 +4,10 @@ PATH=/opt/devkitpro/devkitPPC/bin/:$PATH
 
 powerpc-eabi-gcc -nostdlib -O3 raw.cpp -Wl,-T,link.ld -o raw.o || exit
 powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
-powerpc-eabi-objdump -d raw.o | c++filt
+powerpc-eabi-objdump \
+  --section=.init \
+  --section=.text \
+  -d raw.o
 hexdump \
   -e '4/1 "%02x" " " 4/1 "%02x" "\n"' \
   raw.bin | xsel --clipboard --input
