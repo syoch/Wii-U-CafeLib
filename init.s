3af1b781f6c48263604246dd8249cd0cc47de7d0
syoch <hacksyoch@gmail.com>
2021-07-05 01:14:56 +0900

    update: rodata[0] copy

--- init.s
+++ init.s
@@ -1,13 +1,18 @@
 .section .init
 .global init
+init:
   stwu    1,-8(1)
   mflr    0
 
-  bl      getPC  
+  bl      getPC
+  addi 3, 3, -0x0c+rodata_start
+  ; r3 = init
+
+  lis 9, 0x2000 
   
-  lis     9,8192
-  stw     3,0(9)
-  
+  lwz 10,0(3)
+  stw 10,0(9)
+
   addi    1,1,8
   mtlr    0
   blr
