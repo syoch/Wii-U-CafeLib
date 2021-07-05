e194a5feff1643b3bef27bb5426471da07f53f9b
syoch <hacksyoch@gmail.com>
2021-07-05 17:21:27 +0900

    update: impl init()

--- init.s
+++ init.s
@@ -4,14 +4,22 @@ init:
   stwu    1,-8(1)
   mflr    0
 
-  bl      getPC
-  addi 3, 3, -0x0c+rodata_start
-  ; r3 = init
+  li 8, rodata_end
+  li 9, rodata_start
+  subf 8, 9, 8
+  srwi 8, 8, 2
+  mtctr 8
 
-  lis 9, 0x2000 
-  
-  lwz 10,0(3)
-  stw 10,0(9)
+  bl getPC
+  addi 9, 3, rodata_start - . - 4
+
+  lis 8, 0x1FFF
+  ori 8, 8, 0xFFFC
+
+  loop:
+    lwzu 7, 4(9)
+    stwu 7, 4(8)
+    bdnz loop
 
   addi    1,1,8
   mtlr    0
