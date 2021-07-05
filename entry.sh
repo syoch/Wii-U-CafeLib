2e3ca581accdb6d05435b088ddfed7bb3c2e5d01
syoch <hacksyoch@gmail.com>
2021-07-05 17:36:02 +0900

    update: add data section

--- entry.sh
+++ entry.sh
@@ -10,11 +10,16 @@ powerpc-eabi-gcc \
   -o raw.o \
 || exit
 
-powerpc-eabi-objcopy --only-section=.text raw.o -O binary raw.bin
+powerpc-eabi-objcopy --only-section=.text raw.o -O binary text.bin
+powerpc-eabi-objcopy --only-section=.data raw.o -O binary data.bin
 powerpc-eabi-objdump \
   --section=.init \
   --section=.text \
   -d raw.o
 hexdump \
   -e '4/1 "%02x" " " 4/1 "%02x" "\n"' \
-  raw.bin | xsel --clipboard --input
+  text.bin data.bin | tee dump
+cat dump | xsel --clipboard --input
+rm dump
+rm text.bin data.bin
+exit
