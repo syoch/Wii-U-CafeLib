8c1a9502a1b464f0f0044d060ab7e206c3851398
syoch <hacksyoch@gmail.com>
2021-07-07 21:24:14 +0900

    update: fix add section

--- init.s
+++ init.s
@@ -1,28 +1,4 @@
 .section .init
-.global SetupDatas
-SetupDatas:
-  stwu    1,-8(1)
-  mflr    0
-
-  li 8, rodata_size
-  srwi 8, 8, 2
-  mtctr 8
-
-  bl 4
-  mflr 3
-  addi 9, 3, text_end - . - 4
-
-  lis 8, 0x1FFF
-  ori 8, 8, 0xFFFC
-
-  loop:
-    lwzu 7, 4(9)
-    stwu 7, 4(8)
-    bdnz loop
-
-  addi    1,1,8
-  mtlr    0
-  blr
 
 .global GetTextEnd
 GetTextEnd:
