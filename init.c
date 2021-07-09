bdbd96b47a91b2e0df311d5a8637cb482354e5c6
syoch <hacksyoch@gmail.com>
2021-07-09 16:28:51 +0900

    update: format

--- init.c
+++ init.c
@@ -1,42 +1,39 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
-#pragma GCC optimize("unroll-loops")
-
 void code();
 uint32_t* GetTextEnd();
 extern const void* rodata_start;
 extern const void* rodata_end;
-
-#pragma GCC push_options
+extern void rodata_size;
 
 __attribute__((section(".startup"))) int startup() {
   register uint32_t* src;
   register uint32_t* dest __asm__("r4");
+  register size_t size;
 
   // get data start
   asm volatile(
       "bl 4\n"
       "mflr %[src]\n"
-      "addi %[src], %[src], text_end - . - 8"
+      "addi %[src], %[src], text_end - ."
       :
-      : [src] "r"(src));
+      : [src] "r"(src)
+      : "lr");
   dest = (uint32_t*)(0x20000000) - 1;
+  size = (uint32_t*)(&rodata_size);
 
   // copy to 2000_0000h
   asm volatile(
-      "li 10, rodata_size\n"
-      "mtctr 10\n"
+      "mtctr %[size]\n"
       "Loop1:\n"
       "  lwzu 10, 4(%[src])\n"
       "  stwu 10, 4(%[dest])\n"
       "  bdnz Loop1"
       : [src] "+r"(src), [dest] "+r"(dest)
-      :
-      : "r10");
+      : [size] "r"(size)
+      : "r10", "ctr");
   // code();
 
   return 0;
 }
-
-#pragma GCC pop_options
