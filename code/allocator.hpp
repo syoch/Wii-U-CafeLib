b12d4e0d9d0dc5f0da32647bca92977925f54737
syoch <hacksyoch@gmail.com>
2021-07-17 18:45:23 +0900

    update: impl alloactor

--- code/allocator.hpp
+++ code/allocator.hpp
@@ -4,10 +4,15 @@
 
 extern "C" uint32_t rodata_end;
 inline void* operator new(size_t size) {
-  static char* ptr = (char*)&rodata_end;
-  auto ret = ptr;
-  ptr += size;
-  return ret;
+  register uint8_t* ret;
+  register uint8_t* tmp;
+
+  tmp = *(uint8_t**)(0x20000000);
+  ret = tmp;
+  tmp += 4;
+  *(uint8_t**)(0x20000000) = tmp;
+
+  return (void*)ret;
 }
 inline void* operator new[](size_t size) { return operator new(size); }
 inline void operator delete(void* ptr, size_t size) {}
