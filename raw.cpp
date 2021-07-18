574d060637eab51d79f0547379b72a5e724db5b7
syoch <hacksyoch@gmail.com>
2021-07-18 12:52:38 +0900

    tests: allocator test

--- raw.cpp
+++ raw.cpp
@@ -1,6 +1,9 @@
 #include "code.hpp"
 
 inline void code() {
-  *(float *)(0x20000100) += 1;
+  *(new int) = 1;
+  asm volatile("code_:");
+  *(new float) = 1;
+  // *(new uint32_t) = 1;
   return;
 }
