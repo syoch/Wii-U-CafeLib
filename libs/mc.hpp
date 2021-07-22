c26be53b410fe17ca246bd4dc4c0492a52a89272
syoch <hacksyoch@gmail.com>
2021-07-22 19:01:18 +0900

    update: separete some types

--- libs/mc.hpp
+++ libs/mc.hpp
@@ -1,23 +1,4 @@
-template <typename T>
-struct shared_ptr {
- public:
-  shared_ptr(T *p) {
-    ptr = p;
-    cnt = 0;
-  }
-  shared_ptr() {
-    ptr = 0;
-    cnt = 0;
-  }
-  T *ptr;
-  uint32_t cnt;
-};
-struct vector {
-  uint32_t field_0x0;
-  uint32_t field_0x4;
-  uint32_t field_0x8;
-  uint32_t field_0xc;
-};
+#include "types.hpp"
 class ClientPacketListener {
  public:
   void send(void *packet) {
---
+++ libs/types.hpp
@@ -0,0 +1,22 @@
+#include <inttypes.h>
+
+template <typename T>
+struct shared_ptr {
+ public:
+  shared_ptr(T *p) {
+    ptr = p;
+    cnt = 0;
+  }
+  shared_ptr() {
+    ptr = 0;
+    cnt = 0;
+  }
+  T *ptr;
+  uint32_t cnt;
+};
+struct vector {
+  uint32_t field_0x0;
+  uint32_t field_0x4;
+  uint32_t field_0x8;
+  uint32_t field_0xc;
+};
