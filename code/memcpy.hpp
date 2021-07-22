d0b9f3e7e941997ce5ea3eae639a51bc2c7cd7d3
syoch <hacksyoch@gmail.com>
2021-07-22 18:41:32 +0900

    update: fix memcpy

--- code/memcpy.hpp
+++ code/memcpy.hpp
@@ -4,6 +4,8 @@
 
 template <typename T>
 inline void memcpy(T* src, T* dest, const size_t count) {
+  src--;
+  dest--;
   for (register size_t i = count; i != 0; --i) {
     *(++dest) = *(++src);
   }
